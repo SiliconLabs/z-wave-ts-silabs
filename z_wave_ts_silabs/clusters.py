90624864fb709932c5df63241206faa3e600a45e
Luis Thomas <luis.thomas@silabs.com>
2024-09-28 14:16:26 +0200

    clusters: remove the need for the hacky __post_init__ when loading JSON

--- z_wave_ts_silabs/clusters.py
+++ z_wave_ts_silabs/clusters.py
@@ -9,6 +9,7 @@ class Wpk:
     board: str
     serial: str
 
+
 @dataclass
 class GPIOMapping:
     serial: str
@@ -17,27 +18,44 @@ class GPIOMapping:
     BTN2: int
     BTN3: int
 
+
 @dataclass
 class Rpi:
     hostname: str
     GPIO_mapping: List[GPIOMapping]
 
+
 @dataclass
 class Cluster:
     wpks: List[Wpk]
-    rpis: List[Rpi] = field(default_factory=lambda: []) # this makes rpis optional
-
-    def __post_init__(self):
-        # Only useful if Cluster is created with from_json()
-        for i, elt in enumerate(self.wpks):
-            if type(elt) == dict:
-                self.wpks[i] = Wpk(**elt)
-        for i, elt in enumerate(self.rpis):
-            if type(elt) == dict:
-                self.rpis[i] = Rpi(**elt)
+    rpis: List[Rpi] = field(default_factory=lambda: [])  # this makes rpis optional
 
     @staticmethod
     def from_json(json_file: str, cluster_name: str) -> Cluster:
+        wpks = []
+        rpis = []
+
         with open(json_file, 'r') as f:
             clusters_json = json.load(f)
-        return Cluster(**clusters_json[cluster_name])
+
+        if cluster_name not in clusters_json:
+            raise Exception(f"no cluster named {cluster_name} in {json_file}")
+        cluster_dict = clusters_json[cluster_name]
+
+        if 'wpks' not in cluster_dict:
+            raise Exception(f"no wpks in {json_file}")
+
+        for wpk in cluster_dict['wpks']:
+            wpks.append(Wpk(wpk['board'], wpk['serial']))
+
+        # if the cluster definition does not contain a RPi list then just return now
+        if 'rpi' not in cluster_dict:
+            return Cluster(wpks)
+
+        for rpi in cluster_dict['rpis']:
+            gpio_mapping = []
+            for gpio_mapping in rpi['GPIO_mapping']:
+                gpio_mapping.append(GPIOMapping(**gpio_mapping))
+            rpis.append(Rpi(rpi['hostname'], gpio_mapping))
+
+        return Cluster(wpks, rpis)
