1b73d1b78270686ace1b0d7d4731cedf34ff76b4
Luis Thomas <luis.thomas@silabs.com>
2025-03-05 14:43:19 +0100

    examples: make sure the session_log_dir fixture returns a relative path
    make sure the session_log_dir fixture returns a relative path and not an
    absolute one (the absolute path can be obtained using pathlib.Path.absolute()).
    
    Absolute path tends to be too long to be used with Unix sockets.

--- examples/conftest.py
+++ examples/conftest.py
@@ -5,7 +5,7 @@ from datetime import datetime
 
 @pytest.fixture(scope='session')
 def session_log_dir() -> Path:
-    _session_log_dir = Path.cwd() / 'logs' /  f"{datetime.now().strftime('%Y_%m_%d-%H_%M_%S')}"
+    _session_log_dir = Path('logs' , f"{datetime.now().strftime('%Y_%m_%d-%H_%M_%S')}")
     _session_log_dir.mkdir(parents=True, exist_ok=True)
 
     return _session_log_dir
