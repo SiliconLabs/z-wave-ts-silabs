f55ba3ac3daeda058c24239e7c0f2cec163060da
Xavier Perraud <Xavier.Perraud@silabs.com>
2025-07-23 17:03:27 +0200

    NOJIRA test do not merge

--- z_wave_ts_silabs/fixtures.py
+++ z_wave_ts_silabs/fixtures.py
@@ -95,7 +95,7 @@ def device_factory(updated_session_ctxt: SessionContext, hw_cluster: DevCluster)
 
 @pytest.fixture(scope="function", autouse=True)
 def hw_cluster_neutralize_all_wpk(hw_cluster: DevCluster):
-    parking_region = os.getenv['ZWAVE_PARKING_REGION']
+    parking_region = REGION_IN #os.getenv['ZWAVE_PARKING_REGION']
     if parking_region is not None:
         hw_cluster.neutralize_all_wpk(parking_region)
     yield
